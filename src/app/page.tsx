8caa0d0f879970770cf6207bb3a6965a7b027892
joemama1502 <gogosqeez@gmail.com>
2025-03-30 21:32:48 -0700

    updated homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div style={{ height: '100vh', display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column' }}>
-      <h1>Welcome to My First Next.js Website</h1>
+      <h1>Welcome to My First Actual Website</h1>
       <p>Made by Tony 😎</p>
     </div>
   );
